c56b7e8fa339e00c63b3fe94ed2d1ccadbbf92f4
Robert Maldonado <robert.maldonado@firemon.com>
2024-03-27 19:36:49 -0500

    Update SideNav.tsx

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -4,12 +4,11 @@ import Link from 'next/link';
 
 const items = [
   {
-    title: 'Get started',
     links: [{href: '/docs/Authentication', children: 'Authentication'}],
   },
   {
-      title: 'ParseCSV',
-      links: [{href: '/docs/ParseCSV'}],
+      title: 'Policy Planner',
+      links: [{href: '/docs/ParseCSV', children: 'ParseCSV'}],
     },
 ];
 
